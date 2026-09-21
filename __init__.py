e5f3adcda9d0908f5befee56f6dc46666fc33d2e
amadmnhg-rgb <amadmnhg@gmail.com>
2026-09-21 10:06:37 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""حزمة تطبيق إدارة أسطول الشاحنات والزيوت — صندوق النظافة والتحسين، المهرة."""
-
-__all__ = ["analytics", "config", "oil", "parser", "storage", "styles", "util"]
